842deab9f63109b596b0f1112a428a5d33157e8f
LittleChell <463751313@qq.com>
2020-05-26 17:49:17 +0800

    upload

---
+++ dist/README.md
@@ -0,0 +1,11 @@
+# project
+
+## Project setup
+```
+直接打开index.html即可
+```
+
+```
+使用vue cli搭建的一个页面，配合了element的UI框架，实现了评论、评论折叠、点赞等内容，部分内容存在localstorage里面，部分写在页面变量中
+```
+
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+    html * {
+        outline: 0;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    }
+
+    a,
+    img,
+    label,
+    select,
+    body,
+    dl,
+    dt,
+    dd,
+    ul,
+    ol,
+    li,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    form,
+    input,
+    textarea,
+    p,
+    th,
+    td,
+    hr,
+    button,
+    article,
+    aside,
+    footer,
+    header,
+    menu,
+    nav,
+    section {
+        margin: 0;
+        padding: 0;
+    }
+</style>
